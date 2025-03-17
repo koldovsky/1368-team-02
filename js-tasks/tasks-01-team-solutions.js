7b8a631e3ac52f0e1031457f08c63e388dcf938d
Anastasiia Mishchenko <51370779+anastasia-mishchenko@users.noreply.github.com>
2025-03-17 21:17:23 +0100

    Add first and second tasks JS solution

--- js-tasks/tasks-01-team-solutions.js
+++ js-tasks/tasks-01-team-solutions.js
@@ -1,6 +1,18 @@
 // Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
 // Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+function makeNegative(num) {
+  if (num < 0) {
+    return num;
+  } else {
+    return -num;
+  }
+}
 
 // Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 
