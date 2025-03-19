7d1d410315e604be01755cfb8b0d3ca42193cd87
Anastasiia Mishchenko <51370779+anastasia-mishchenko@users.noreply.github.com>
2025-03-19 15:38:06 +0100

    Add JS solutions

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -1,11 +1,63 @@
 // Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
-// Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
-
-// Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
-
-// Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
-
-// Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
-
-// Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
+  
+  // Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+  
+  function makeNegative(num) {
+    if (num < 0) {
+      return num;
+    } else {
+      return -num;
+    }
+  }
+  
+  function makeNegative(num) {
+    return num > 0 ? -num : num;
+  }
+  
+  // Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+  
+  function move (position, roll) {
+    return position + roll * 2;
+  }
+  
+  // Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+  
+  function greet (name, owner) {
+    return name === owner ? "Hello boss" : "Hello guest";
+  }
+  
+  function greet (name, owner) {
+      if (name === owner){
+        return "Hello boss"
+      }else{
+        return "Hello guest"
+      }
+    }
+  
+  // Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+  
+  function litres(time) {
+    return parseInt(time * 0.5);
+  }
+  
+  function litres(time) {
+    return Math.floor(time * 0.5);
+  }
+  
+  // Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+  
+  function lovefunc(flower1, flower2){
+    return !! (flower1 % 2 > 0 ^ flower2 % 2 > 0);
+  }
+  
+  function lovefunc(flower1, flower2){
+      if ((flower1 % 2 === 0 && flower2 % 2 !== 0) || (flower1 % 2 !== 0 && flower2 % 2 === 0)) {
+            return true; 
+        } else {
+            return false; 
+        }
+    }
