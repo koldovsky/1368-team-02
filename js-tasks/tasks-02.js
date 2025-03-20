f8a8d81d59a81316b2d7b76c7ad1ab52400f0db0
dmytro-gthb <dima.nesvizh90@gmail.com>
2025-03-20 22:00:58 +0000

    Add last JS solution

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,32 +1,3 @@
-//Базові
-// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-
-function stringToArray(string) {
-  return string.split(" ");
-}
-
-//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
-
-function DNAtoRNA(dna) {
-  return dna.replaceAll("T", "U");
-}
-
-//Поглиблені
-//https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
-
-var min = function (list) {
-  list.sort((a, b) => a - b);
-  return list[0];
-};
-
-var max = function (list) {
-  list.sort((a, b) => b - a);
-  return list[0];
-};
-
-
-
-
 //https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 
 function min(arr, toReturn) {
@@ -54,8 +25,8 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
 
 //Return n-th even number    https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
 
-function nthEven(n){
-  return n * 2 - 2; 
+function nthEven(n) {
+  return n * 2 - 2;
 }
 
 //What's the real floor     https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
