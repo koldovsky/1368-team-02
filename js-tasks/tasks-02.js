39c474fb8ac6293723e32164f295b75762a1ba1a
Anastasiia Mishchenko <51370779+anastasia-mishchenko@users.noreply.github.com>
2025-03-20 16:28:26 +0100

    Add JS solutions

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,41 +1,43 @@
 //Базові
 // https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 
-
+function stringToArray(string) {
+  return string.split(" ");
+}
 
 //https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 
-
+function DNAtoRNA(dna) {
+  return dna.replaceAll("T", "U");
+}
 
 //Поглиблені
 //https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+var min = function (list) {
+  list.sort((a, b) => a - b);
+  return list[0];
+};
+
+var max = function (list) {
+  list.sort((a, b) => b - a);
+  return list[0];
+};
+
 
 
 
 //https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 
-
 //Додаткові задачки по JS, щоб формувати навички, якщо маєте час і бажання
 //Double Integer    https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
 
-
 //Twice as old    https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
-
-
 
 //Return n-th even number    https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
 
-
-
 //What's the real floor     https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
-
-
 
 //Clock    https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
 
-
-
 //Is n divisible by x and y    https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
-
-
-
