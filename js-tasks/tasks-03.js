71662ad2fe8f5d024a70d2177dd80aa26d127e22
Anastasiia Mishchenko <51370779+anastasia-mishchenko@users.noreply.github.com>
2025-03-24 15:53:02 +0100

    Add JS solution

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -23,5 +23,17 @@ function giveMeFive(obj) {
 
 //опційна поглиблена задачка про замикання (дивіться відео від мене спочатку https://youtu.be/XJdzZgzLc08)
 //https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+
+function buildFun(n) {
+  const res = [];
+
+  for (let i = 0; i < n; i++) {
+    res.push(function () {
+      return i;
+    });
+  }
+  return res;
+}
+
 //опційна поглиблена задачка про наслідування
 //https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
