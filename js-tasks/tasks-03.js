8bdb3760ed98305a74d2178db9577caef37dbb92
Anastasiia Mishchenko <51370779+anastasia-mishchenko@users.noreply.github.com>
2025-03-24 15:41:46 +0100

    Update tasks-03.js
    Update tasks-03.js

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -1,7 +1,7 @@
 //https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
 
 function circleCircumference(circle) {
-  return (result = 2 * Math.PI * circle.radius);
+  return 2 * Math.PI * circle.radius;
 }
 
 //https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
