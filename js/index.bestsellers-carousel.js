a6f52813fd4aa6382fd12e705842a6806762f3fe
dmytro-gthb <dima.nesvizh90@gmail.com>
2025-04-03 19:08:56 +0000

    Improve responsiveness of bestsellers carousel

--- js/index.bestsellers-carousel.js
+++ js/index.bestsellers-carousel.js
@@ -1,25 +1,37 @@
 const slides = [
-  `<div class="bestsellers-carousel-image__wrapper">
-    <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_basic.png">
+  `<div class="bestsellers-carousel__item">
+    <div class="bestsellers-carousel__image-wrapper">
+      <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_basic.png" alt="Basic">
+    </div>
     <p class="bestsellers-carousel__label">Basic</p>
     <button class="bestsellers-carousel__browse-button" id="bestseller-1">Buy</button>
   </div>`,
-  `<div class="bestsellers-carousel-image__wrapper">
-    <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_limited.png">
+
+  `<div class="bestsellers-carousel__item">
+    <div class="bestsellers-carousel__image-wrapper">
+      <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_limited.png" alt="Limited">
+    </div>
     <p class="bestsellers-carousel__label">Limited</p>
     <button class="bestsellers-carousel__browse-button" id="bestseller-2">Buy</button>
   </div>`,
-  `<div class="bestsellers-carousel-image__wrapper">
-    <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_sets.png">
+
+  `<div class="bestsellers-carousel__item">
+    <div class="bestsellers-carousel__image-wrapper">
+      <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_sets.png" alt="Sets">
+    </div>
     <p class="bestsellers-carousel__label">Sets</p>
     <button class="bestsellers-carousel__browse-button" id="bestseller-3">Buy</button>
   </div>`,
-  `<div class="bestsellers-carousel-image__wrapper">
-    <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_swimwear.png">
+
+  `<div class="bestsellers-carousel__item">
+    <div class="bestsellers-carousel__image-wrapper">
+      <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_swimwear.png" alt="Swimwear">
+    </div>
     <p class="bestsellers-carousel__label">Swimwear</p>
     <button class="bestsellers-carousel__browse-button" id="bestseller-4">Buy</button>
   </div>`
 ];
+
 
 const products = [
   { id: "bestseller-1", name: "Basic Lingerie", price: "49.99$", image: "img/bestsellers-carousel/bestsellers_carousel_basic.png" },
