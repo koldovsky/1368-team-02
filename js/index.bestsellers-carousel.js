159f9539e4db98a4f63b691928a9c97d76e4a71d
dmytro-gthb <dima.nesvizh90@gmail.com>
2025-04-04 10:25:46 +0000

    Add hover effect for slides in bestsellers carousel

--- js/index.bestsellers-carousel.js
+++ js/index.bestsellers-carousel.js
@@ -1,36 +1,46 @@
 const slides = [
   `<div class="bestsellers-carousel__item">
     <div class="bestsellers-carousel__image-wrapper">
-      <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_basic.png" alt="Basic">
+      <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_basic.png" alt="Basic" />
+      <div class="bestsellers-carousel__hover">
+        <button class="bestsellers-carousel__browse-button" id="bestseller-1">Buy</button>
+      </div>
     </div>
     <p class="bestsellers-carousel__label">Basic</p>
-    <button class="bestsellers-carousel__browse-button" id="bestseller-1">Buy</button>
   </div>`,
 
   `<div class="bestsellers-carousel__item">
     <div class="bestsellers-carousel__image-wrapper">
-      <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_limited.png" alt="Limited">
+      <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_limited.png" alt="Limited" />
+      <div class="bestsellers-carousel__hover">
+        <button class="bestsellers-carousel__browse-button" id="bestseller-2">Buy</button>
+      </div>
     </div>
     <p class="bestsellers-carousel__label">Limited</p>
-    <button class="bestsellers-carousel__browse-button" id="bestseller-2">Buy</button>
   </div>`,
 
   `<div class="bestsellers-carousel__item">
     <div class="bestsellers-carousel__image-wrapper">
-      <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_sets.png" alt="Sets">
+      <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_sets.png" alt="Sets" />
+      <div class="bestsellers-carousel__hover">
+        <button class="bestsellers-carousel__browse-button" id="bestseller-3">Buy</button>
+      </div>
     </div>
     <p class="bestsellers-carousel__label">Sets</p>
-    <button class="bestsellers-carousel__browse-button" id="bestseller-3">Buy</button>
   </div>`,
 
   `<div class="bestsellers-carousel__item">
     <div class="bestsellers-carousel__image-wrapper">
-      <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_swimwear.png" alt="Swimwear">
+      <img class="bestsellers-carousel__image" src="img/bestsellers-carousel/bestsellers_carousel_swimwear.png" alt="Swimwear" />
+      <div class="bestsellers-carousel__hover">
+        <button class="bestsellers-carousel__browse-button" id="bestseller-4">Buy</button>
+      </div>
     </div>
     <p class="bestsellers-carousel__label">Swimwear</p>
-    <button class="bestsellers-carousel__browse-button" id="bestseller-4">Buy</button>
   </div>`
 ];
+
+
 
 
 const products = [
